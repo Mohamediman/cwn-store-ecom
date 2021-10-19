6e7d47e589cd0a9dab983ea3843b979d83d27119
mohamediman <imansaabir@gmail.com>
2021-10-18 19:18:56 -0600

    initial commit

--- app.js
+++ app.js
@@ -33,12 +33,15 @@ DBConnect();
 app.use("/api/v1/users", usersRouter);
 
 //===Set static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+// app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "index.html"));
+});
+
+// }
 
 //========== Listen the port =============
 const port = process.env.PORT || 8000;
